11da9d28a5b67b65ea9034cc70c18699aea4f046
astalberto <albertoherrera205@hotmail.com>
2024-04-11 17:14:52 -0500

    Push 02

--- Demo/src/paquete2/Demo002.java
+++ Demo/src/paquete2/Demo002.java
@@ -9,6 +9,7 @@ package paquete2;
  *
  * @author reroes
  */
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import paquete1.Mensaje;
 
@@ -18,18 +19,27 @@ public class Demo002 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Para poder importar un paquete utilizamos "import" en adicion de la ruta
-        // del paquete y de la clase a usar para poder usar el procedimiento o funcion
+        // Para poder importar un paquete utilizamos "import" en adicion de la
+        // ruta del paquete y de la clase a usar para poder usar el 
+        // procedimiento o funcion
         Scanner entrada = new Scanner(System.in);
         int check;
         String msg = Mensaje.obtenerMensaje();
-        System.out.println("Ingrese 1/2/3/4/5 si desea imprimir el mensaje original");
-        check = entrada.nextInt();
-        // 12345 original // otro Mayus
-        if (check >= 1 && check <= 5) {
-            System.out.printf("%s\n", msg);
-        } else {
-            System.out.printf("%s\n", msg.toUpperCase());
+        try {
+            System.out.println("Ingrese 1/2/3/4/5 si desea imprimir el mensaje "
+                    + "original");
+            check = entrada.nextInt();
+            // Si el numero ingresado esta entre 1 2 3 4 5 se imprime el Sring 
+            // original 
+            // Si el numero ingresado esta fuera de los limites se escribe el 
+            // String en Mayusculas
+            if (check >= 1 && check <= 5) {
+                System.out.printf("%s\n", msg);
+            } else {
+                System.out.printf("%s\n", msg.toUpperCase());
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Valor decimal no admitido");
         }
     }
 
